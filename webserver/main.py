5e08fc637a5054b707cde40dae76b23c2f819988
saimonproyectsv2 <simon.patino@udea.edu.co>
2022-12-21 19:28:10 -0500

    add web sever with uvicorn and fastapi

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,15 @@
 import store
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,3,4]
+
+@app.get("/")
+def get_list():
+    return {name: "Platzi"}
 
 def run():
     store.get_categories()
