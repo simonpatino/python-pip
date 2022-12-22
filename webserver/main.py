5b55ec657a3db2a0ab0937d2faae33d966bbe1aa
saimonproyectsv2 <simon.patino@udea.edu.co>
2022-12-21 19:55:43 -0500

    responsive html

--- webserver/main.py
+++ webserver/main.py
@@ -1,15 +1,25 @@
 import store
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse 
+
 app = FastAPI()
 
 @app.get("/")
 def get_list():
     return [1,2,3,4]
 
-@app.get("/contact")
+@app.get("/contact", response_class=HTMLResponse)
 def get_list():
-    return {"name": "Platzi"}
+    return """ 
+    
+    
+    <h1> Hello world from uvicorn </h1>
+    <p>Nothing more to see here ...</p>   
+    
+    
+    
+    """
 
 def run():
     store.get_categories()
